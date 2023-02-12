b282f9d5f716b395427098c06129f0375a7a4f71
DDubicki <dubickidymitr@gmail.com>
2023-02-12 18:17:22 +0100

    Amigos-1 Initialising

---
+++ src/main/java/amigos_practice/main/AmigosApplication.java
@@ -0,0 +1,13 @@
+package amigos_practice.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosApplication.class, args);
+	}
+
+}
